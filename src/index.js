72de47fc65c694c2ccde0f9ffc48e70e4d95bc13
Nicholas <bestnicholas05@gmail.com>
2024-07-23 00:26:26 -0400

    Completed Ramen Code Challenge

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
 // index.js
+const ramenAPI = 'http://localhost:3000/ramens'
+const ramenMenuDiv = el('ramen-menu')
+// const newRamenForm = el('new-ramen')
+// or instead write
+el('new-ramen').addEventListener('submit', addSubmitListener)
 
-// Callbacks
-const handleClick = (ramen) => {
-  // Add code
-};
+fetch(ramenAPI)
+    .then(res => res.json())
+    .then(ramens => renderRamens(ramens))
+    // You can also write
+    // .then(renderRamens)
 
-const addSubmitListener = () => {
-  // Add code
+function renderRamens(ramens) {
+    ramens.forEach(renderRamen)
+}
+
+function renderRamen(ramen) {
+    const ramenImageElement = document.createElement('img')
+    ramenImageElement.src = ramen.image
+    ramenMenuDiv.append(ramenImageElement)
+
+    ramenImageElement.addEventListener('click', () => handleClick(ramen))
+}
+
+function handleClick(ramen) {
+  el('detail-image').src = ramen.image
+  el('detail-name').textContent = ramen.name
+  el('detail-restaurant').textContent = ramen.restaurant 
+  el('rating-display').textContent = ramen.rating
+  el('comment-display').textContent = ramen.comment 
+}
+
+function addSubmitListener(event) {
+  event.preventDefault()
+  const newRamen = {
+      name: event.target.name.value,
+      restaurant: event.target.restaurant.value,
+      image: event.target.image.value,
+      rating: event.target.rating.value,
+      comment: event.target['new-comment'].value, 
+
+  }
+
+    renderRamen(newRamen)
+    event.target.reset()
+}
+
+function el(id) {
+  return document.getElementById(id)
+  // You can also write 
+  // return document.querySelector(`${id}`)
 }
 
 const displayRamens = () => {
